0b6f193c40cc61190cdaa145b4cafc097537cbb1
Davide Crestini <davide.crestini94@gmail.com>
2019-05-16 11:11:46 +0100

    added more ingredients categoire

--- src/valueobjects/IngredientsCategories.ts
+++ src/valueobjects/IngredientsCategories.ts
@@ -4,6 +4,12 @@ const TYPES = {
   'ic-1': 'Dairy',
   'ic-2': 'Vegetables',
   'ic-3': 'Poultry',
+  'ic-4': 'Meat',
+  'ic-5': 'Soft Drink',
+  'ic-6': 'Alcoholic Drink',
+  'ic-7': 'Condiment',
+  'ic-8': 'Flour',
+  'ic-9': 'Grain',
 };
 class IngredientsCategories extends ValueObject {
   constructor() {
