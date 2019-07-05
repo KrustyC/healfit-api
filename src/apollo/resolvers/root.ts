d59391e03f2ee107533ec9a3eccff9208f204210
Davide Crestini <davide.crestini94@gmail.com>
2019-07-05 15:37:32 +0100

    fixed some issuses around meal plan schemas

--- src/apollo/resolvers/root.ts
+++ src/apollo/resolvers/root.ts
@@ -3,6 +3,8 @@ import MealTypes from '@valueobjects/MealTypes';
 import Measurements from '@valueobjects/Measurements';
 import RecipeCategories from '@valueobjects/RecipeCategories';
 import RecipeLevels from '@valueobjects/RecipeLevels';
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
 
 const globalData = {
   ingredientsCategories: IngredientsCategories.indexedList(),
@@ -13,6 +15,22 @@ const globalData = {
 };
 
 export default {
+  Date: new GraphQLScalarType({
+    description: 'Date custom scalar type',
+    name: 'Date',
+    parseValue(value) {
+      return new Date(value); // value from the client
+    },
+    serialize(value) {
+      return value.getTime(); // value sent to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return parseInt(ast.value, 10); // ast value is always in string format
+      }
+      return null;
+    },
+  }),
   Mutation: {
     hello: () => 'Hello! Mutations are working!',
   },
